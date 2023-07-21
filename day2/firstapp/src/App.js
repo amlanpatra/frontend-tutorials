229b8cdb706ba9b681e95237d95161d823db8631
amlanpatra <amlanpatra16@gmail.com>
2023-07-21 20:05:15 +0530

    components added

--- day2/firstapp/src/App.js
+++ day2/firstapp/src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import Box from "./Box";
 
 function App() {
   return (
     <div className="App">
       <div className="grid place-content-center h-screen border-[20px] border-red-500">
-        Hi there! How are you?
+        <div className="flex">
+          <Box text={"hello"} />
+          <Box text={"how"} />
+          <Box text={"are"} />
+          <Box text={"you"} />
+        </div>
       </div>
     </div>
   );
---
+++ day2/firstapp/src/Box.jsx
@@ -0,0 +1,5 @@
+function Box({ text }) {
+  return <div className="border-2 border-red-600">{text}</div>;
+}
+
+export default Box;
