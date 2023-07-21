5326e18f67bf2c5b607b59f7b4c00853ead5a8f3
amlanpatra <amlanpatra16@gmail.com>
2023-07-21 19:27:36 +0530

    initial commit

---
+++ day2/firstapp/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="grid place-content-center h-screen border-[20px] border-red-500">
+        Hi there! How are you?
+      </div>
+    </div>
+  );
+}
+
+export default App;
