c9fef29960612e14b664d5f7db44023c3ad943d5
Amlan Patra <amlanpatra@gmail.com>
2023-07-23 19:56:04 +0530

    useState1 added

--- day3/todoapp/src/App.js
+++ day3/todoapp/src/App.js
@@ -1,11 +1,29 @@
 import "./App.css";
 import Task from "./Task";
+import { useState } from "react";
+
 function App() {
+  const [taskItem, setTaskItem] = useState("hello");
+
   return (
     <div className="App flex-row border-white border-[10px] w-screen">
+      <input
+        className="w-full h-12 p-2 rounded-md border-slate-300 border-[1px] mb-2"
+        type="text"
+        placeholder="add item..."
+        value={taskItem}
+        onChange={(e) => setTaskItem(e.target.value)}
+      />
+
+      <button
+        className="h-fit w-fit bg-black text-white p-2 rounded-md mb-2"
+        onClick={() => alert(taskItem)}
+      >
+        ADD
+      </button>
       <Task taskDetails={"Task 1"} />
       <Task taskDetails={"Task 2"} />
-      <Task taskDetails={"Task 3"} />
+      <Task taskDetails={"Task 4"} />
     </div>
   );
 }
